e77f57f5de9cc84a6fb7c26b6159f884de068fae
DumDumGeniuss <dumdumgenius@gmail.com>
2015-12-23 15:48:50 +0800

    Fix the wrong sequence in articles show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
   end
   
   def by_topic_id
-    @articles = Article.select("users.nickname,articles.*").from("users,articles").where(["users.id=articles.user_id AND articles.topic_id = ?",params[:topic_id]])
+    @articles = Article.select("users.nickname,articles.*").from("users,articles").where(["users.id=articles.user_id AND articles.topic_id = ?",params[:topic_id]]).order(:created_at)
     #@articles = Topic.find(params[:topic_id]).articles.joins(:users).on("users.id=articles.user_id")
     @topic = Topic.find(params[:topic_id])
     render json: {
